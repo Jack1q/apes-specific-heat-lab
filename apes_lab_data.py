674b5442c7355ad2bd58f74724bec1c951835b0e
Jack Donofrio <johndonofrio2021@students.sjcadets.org>
2021-01-16 23:12:26 -0500

    added option to show fit lines

--- apes_lab_data.py
+++ apes_lab_data.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plot
+import numpy
+
+def mean(list):
+    return sum(list) / len(list)
+
+def least_squares_regression_slope(x, y):
+    numerator = sum([x[i] * y[i] for i in range(len(x))]) - mean(y) * sum(x)
+    denominator = sum([i*i for i in x]) - mean(x) * sum(x)
+    return numerator / denominator
+
+def least_squares_intercept(x, y):
+    numerator = mean(y) * sum([i*i for i in x]) - mean(x) * sum([x[i] * y[i] for i in range(len(x))])
+    denominator = sum([i*i for i in x]) - mean(x) * sum(x)
+    return numerator / denominator
+
+def least_squares_regression_equation(x, y):
+    return least_squares_regression_slope(x, y) * numpy.array(x) + least_squares_intercept(x, y)
 
 # X var
 times = [x for x in range(0, 630, 30)] # 0 - 600, incremented by 30
@@ -12,7 +29,14 @@ soil_temps = [21.4, 21.7, 22.2, 23, 23.8, 24.8, 25.8, 26.8, 27.8, 28.9, 30, 31, 
 plot.scatter(x=times, y=water_temps, label='Water', color='blue')
 plot.scatter(x=times, y=black_sand_temps, label='Black Sand', color='black')
 plot.scatter(x=times, y=white_sand_temps, label='White Sand', color='tan')
-plot.scatter(x=times, y=soil_temps, label='Soil', color='brown')
+plot.scatter(x=times, y=soil_temps, label='Soil', color='brown')    
+
+# # uncomment to show regression lines
+# plot.plot(times, least_squares_regression_equation(times, water_temps), 'r', color='blue',alpha=0.5)
+# plot.plot(times, least_squares_regression_equation(times, black_sand_temps), 'r', color='black',alpha=0.5)
+# plot.plot(times, least_squares_regression_equation(times, white_sand_temps), 'r',color='tan',alpha=0.5)
+# plot.plot(times, least_squares_regression_equation(times, soil_temps), 'r',color='brown',alpha=0.5)
+
 plot.xlabel('Time (s)')
 plot.ylabel("Temperature (\N{DEGREE SIGN}C)")
 plot.ylim(bottom=10, top=120)
